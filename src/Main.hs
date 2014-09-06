bb24f5affdda2b4e2dd2f980ac1d97215fa4afb0
Cerek Hillen <crockeo@gmail.com>
2014-09-06 10:54:12 -0400

    Constructed a little example. Going to expand on it more later.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
+--------------------
+-- Global Imports --
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLFW as GLFW
+import Control.Monad
+import Data.IORef
+
+-------------------
+-- Local Imports --
+import Network
+import Math
+
 ----------
 -- Code --
 
@@ -7,4 +19,18 @@ module Main where
   The main function - the entry point of the program.
 -}
 main :: IO ()
-main = putStrLn "Nothing here yet."
+main = do
+  inited <- initialize
+  unless inited $ error "Could not initialize GLFW."
+
+  opened <- openWindow (Size 640 480) [DisplayRGBBits 8 8 8, DisplayAlphaBits 8, DisplayDepthBits 24] Window
+  unless opened $ error "Could not open the GLFW window."
+
+  closedRef <- newIORef False
+  windowCloseCallback $= do
+    writeIORef closedRef True
+    return True
+
+  runNetwork closedRef
+
+  closeWindow
---
+++ src/Network.hs
@@ -0,0 +1,69 @@
+module Network (runNetwork) where
+
+--------------------
+-- Global Imports --
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLFW as GLFW
+import Control.Wire
+import Data.IORef
+
+-------------------
+-- Local Imports --
+import Renderable
+import Math
+
+----------
+-- Code --
+
+{-|
+  The speed in which the dot should move.
+-}
+speed :: Float
+speed = 0.1
+
+{-|
+  The time since the program started.
+-}
+timeFloat :: HasTime t s => Wire s e IO a Float
+timeFloat = timeF
+
+{-|
+  The permanant position of the dot.
+-}
+initPos :: Wire s e IO a (Vector Float)
+initPos = pure $ pure 0
+
+{-|
+  The moving position of the dot.
+-}
+movPos :: HasTime t s => Wire s e IO a (Vector Float)
+movPos = liftA2 (^+>) initPos $ fmap (* speed) timeFloat
+
+{-|
+  The internal function for running the network.
+-}
+runNetwork' :: (HasTime t s, Renderable b) => IORef Bool -> Session IO s -> Wire s e IO a b -> IO ()
+runNetwork' closedRef session wire = do
+  closed <- readIORef closedRef
+  if closed
+    then return ()
+    else do
+      pollEvents
+
+      (st, session') <- stepSession session
+      (wt, wire'   ) <- stepWire wire st (Right undefined)
+
+      case wt of
+        Left  _     -> return ()
+        Right state -> do
+          clear [ColorBuffer]
+          render state
+          swapBuffers
+          runNetwork' closedRef session' wire'
+
+{-|
+  Running the network.
+-}
+runNetwork :: IORef Bool -> IO ()
+runNetwork closedRef =
+  runNetwork' closedRef clockSession_ movPos
